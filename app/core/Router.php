8741a5ec1c71888a57f23979008811756dbea71b
Fenr1er <michael-koller@outlook.com>
2024-04-05 12:06:50 +0200

    comments - with luki suggestion - regex - initialisierung

--- app/core/Router.php
+++ app/core/Router.php
@@ -63,22 +63,43 @@ class Router
         }
     }
 
-
+    // Gibt die zusätzlichen Daten zurück, die im Routing-Prozess gesammelt wurden.
     private function matchSimpleRoute(Route $route, string $server_uri, string $server_method): bool
     {
+        // aktuelle Request Methode finden.
         $routeUri = $route->uri;
-
+        
+        // Überprüft, ob die URI der Route mit der Anfrage übereinstimmt.
         if (!empty($server_uri)) {
+            // ersetzen des ersten "/" mit nichts in Wert $_SERVER["REQUEST_URI"])['path']
             $routeUri = preg_replace("/(^\/)|(\/$)/", "", $route->uri);
+            // ersetzen des ersten "/" mit nichts in Wert $_SERVER["REQUEST_URI"])['path']
             $reqUri = preg_replace("/(^\/)|(\/$)/", "", $server_uri);
         } else {
+            // Wenn die URI leer ist, wird ein "/" als Standardwert verwendet.
             $reqUri = "/";
         }
-
+        // Überprüft, ob die URI der Route mit der Anfrage übereinstimmt und ob die HTTP-Methode übereinstimmt.
         return $reqUri == $routeUri && $route->method == $server_method;
-    } 
-    
- 
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                              KomplizierterTeil                             */
+    /* -------------------------------------------------------------------------- */
+    /**
+     * da die URI der Route und die URI der Anfrage nicht immer identisch sind, müssen wir die Parameter in der URI der Route mit den Werten in der URI der Anfrage abgleichen.
+     * Dazu verwenden wir reguläre Ausdrücke, um die Parameter in der URI der Route zu finden und die Werte in der URI der Anfrage zu extrahieren.
+     * Wenn die Parameter in der URI der Route gefunden werden, ersetzen wir sie durch reguläre Ausdrücke und vergleichen sie mit der URI der Anfrage.
+     * Wenn die URI der Route und die URI der Anfrage übereinstimmen, wird die Route als passend betrachtet.
+     * Wenn die Route passt, werden die zusätzlichen Daten in einem Array gespeichert, das später im Controller verwendet werden kann.
+     * @param Route $route
+     * @return bool
+     * @throws \Exception
+     * @throws \Throwable
+     * @throws \Error
+     * @throws \ErrorException
+     */
+
     private function matchRoutes(Route $route): bool
     {
         //* aktuelle Request Methode finden.
@@ -89,23 +110,27 @@ class Router
             return false;
         }
 
-        
+
         //! ersetzen des ersten "/" mit nichts in Wert $_SERVER["REQUEST_URI"])['path']
         $server_uri = preg_replace("/(^\/)|(\/$)/", "", parse_url($_SERVER["REQUEST_URI"])['path']);
         parse_str($_SERVER['QUERY_STRING'], $queries);
 
-        //! anlegen deer Zwei Arrays
+        //! anlegen der Zwei Arrays
         $params = [];
         $paramKey = [];
-        
 
+        //! überprüft ob direkt nach dem "/" in der URI von routes.php ein "{" kommt. danach kann stehen was will. danach muss ein "}" kommen und direkt wieder ein "/".
+        //!  Dies verwenden wir un den {slug} zu bekommen
         preg_match_all("/(?<={).+?(?=})/", $route->uri, $paramMatches);
-
+        //! Überprüfuung ob $paramMatches leer ist
+        //! wenn es leer ist wird die funktion matchSimpleRoute() aufgerufen
         if (empty($paramMatches[0])) {
+            //! compact() erstellt ein Array aus Variablen und deren Werten
             $this->data = compact("queries");
             return $this->matchSimpleRoute($route, $server_uri, $server_method);
         }
-
+        //! wenn $paramMatches nicht leer ist wird die foreach schleife ausgeführt
+        //! 
         foreach ($paramMatches[0] as $key) {
             $paramKey[$key] = $key;
         }
@@ -116,37 +141,43 @@ class Router
         } else {
             $reqUri = "/";
         }
-
+        //! aufteilen der route aus routes. jeder teil der URL (durch / getrennt) wird als arrayeintrag eingefügt
         $uri = explode("/", $routeUri);
 
         $indexNum = [];
 
+        //! jeder teil des uri arrays wird dursucht
         foreach ($uri as $index => $param) {
             if (preg_match("/{.*}/", $param)) {
                 $indexNum[] = $index;
             }
         }
-
+        //! aufteilen der route des Users. jeder teil der URL (durch / getrennt) wird als arrayeintrag eingefügt
         $reqUri = explode("/", $reqUri);
 
+        //! für jeden gefundenen parameter wird überprüft ob eine einegabe des Users dazu passt -> localhost/products/{slug} in routes | localhost/products/Tischdecke
         foreach ($indexNum as $key => $index) {
             if (empty($regUri[$index])) {
                 return false;
             }
-
+            //! wenn der {slug} platzhalter mit der vom User eingegebenen URL (an dieser stelle ) eingesetzt wurde weise statt {slug} den entsprechenden parameter zu
             $params[$paramKey[$key]] = $reqUri[$index];
+
+
             $reqUri[$index] = "{.*}";
         }
-
+        //! erzuegen eines strings aus dem array $reqUri
         $reqUri = implode("/", $reqUri);
+        //! ersetzen der / durch \\
         $reqUri = str_replace("/", '\\/', $reqUri);
 
-        if(preg_match("/$reqUri/", $routeUri)) {
+        //! 
+        if (preg_match("/$reqUri/", $routeUri)) {
+            //! compact() erstellt ein Array aus Variablen und deren Werten
             $this->data = compact("queries", "params");
             return true;
         }
 
         return false;
     }
-} 
-
+}
